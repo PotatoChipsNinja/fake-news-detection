3413139ea5071f797f790e27426b5ce4f59bccc8
Yuhui Shi <shiyuhui2000@126.com>
2022-01-06 10:00:48 +0800

    update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from torch import nn, optim
 from transformers import BertModel
 from tqdm import tqdm
 
-from utils import Averager
+from utils import Averager, metrics
 
 class BERTModel(nn.Module):
     def __init__(self, pretrain_dir):
@@ -20,6 +20,7 @@ class BERTModel(nn.Module):
     def forward(self, input_ids, attention_mask):
         feature = self.bert(input_ids, attention_mask).last_hidden_state[:, 0]
         output = self.linear_relu_stack(feature)
+        output = torch.sigmoid(output)
         return output
 
 class Trainer:
@@ -30,11 +31,12 @@ class Trainer:
         self.val_dataloader = val_dataloader
         self.test_dataloader = test_dataloader
         self.model = BERTModel(pretrain_dir).to(device)
-        self.criterion = nn.BCEWithLogitsLoss()
+        self.criterion = nn.BCELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
     def train(self):
         for epoch in range(self.epoch):
+            print('----epoch %d----' % epoch)
             self.model.train()
             avg_loss = Averager()
             for i, batch in enumerate(tqdm(self.train_dataloader)):
@@ -48,7 +50,23 @@ class Trainer:
                 loss.backward()
                 self.optimizer.step()
                 avg_loss.add(loss.item())
-            print('epoch %d: loss = %.4f' % (epoch+1, avg_loss.get()))
+            results = self.test(self.val_dataloader)
+            print('epoch %d: loss = %.4f, acc = %.4f, f1 = %.4f, auc = %.4f' % (epoch+1, avg_loss.get(), results['accuracy'], results['f1'], results['auc']))
+        print('----test----')
+        results = self.test(self.test_dataloader)
+        print('test: acc = %.4f, f1 = %.4f, auc = %.4f' % (results['accuracy'], results['f1'], results['auc']))
 
     def test(self, dataloader):
-        pass
+        self.model.eval()
+        y_true = torch.empty(0)
+        y_score = torch.empty(0)
+        for i, batch in enumerate(tqdm(dataloader)):
+            input_ids = batch['input_ids'].to(self.device)
+            attention_mask = batch['attention_mask'].to(self.device)
+            with torch.no_grad():
+                output = self.model(input_ids, attention_mask)
+            output = output.squeeze().cpu()
+            y_score = torch.cat((y_score, output))
+            label = batch['label']
+            y_true = torch.cat((y_true, label))
+        return metrics(y_true, y_score)
