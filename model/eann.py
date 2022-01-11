26f5837bad75116b5dc00a1861dc4d2b7d8453e9
PCNinja <shiyuhui2000@126.com>
2022-01-11 12:51:39 +0800

    add functions.py

--- model/eann.py
+++ model/eann.py
@@ -4,47 +4,8 @@ from torch import nn, optim
 from transformers import BertModel
 from tqdm import tqdm
 
+from functions import CNNExtractor, MLP, ReverseLayerF
 from utils import Averager, Recorder, split, metrics
-
-class CNNExtractor(nn.Module):
-    def __init__(self, feature_kernel, input_dim):
-        super(CNNExtractor, self).__init__()
-        self.convs = nn.ModuleList([nn.Conv1d(input_dim, feature_num, kernel_size) for kernel_size, feature_num in feature_kernel.items()])
-
-    def forward(self, input):
-        input = input.permute(0, 2, 1)
-        feature = [conv(input) for conv in self.convs]
-        feature = [torch.max_pool1d(f, f.shape[-1]).squeeze() for f in feature]
-        feature = torch.cat(feature, dim=1)
-        return feature
-
-class MLP(nn.Module):
-    def __init__(self, input_dim, hidden_dims, output_dim, dropout):
-        super(MLP, self).__init__()
-        layers = list()
-        curr_dim = input_dim
-        for hidden_dim in hidden_dims:
-            layers.append(nn.Linear(curr_dim, hidden_dim))
-            layers.append(nn.BatchNorm1d(hidden_dim))
-            layers.append(nn.ReLU())
-            layers.append(nn.Dropout(p=dropout))
-            curr_dim = hidden_dim
-        layers.append(nn.Linear(curr_dim, output_dim))
-        self.mlp = nn.Sequential(*layers)
-
-    def forward(self, input):
-        return self.mlp(input)
-
-class ReverseLayerF(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input, alpha):
-        ctx.alpha = alpha
-        return input.view_as(input)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        output = grad_output.neg() * ctx.alpha
-        return output, None
 
 class Model(nn.Module):
     def __init__(self, task_num, feature_kernel={1: 64, 2: 64, 3: 64, 5: 64, 10: 64}, hidden_dims=[512], dropout=0.2, alpha=1):
