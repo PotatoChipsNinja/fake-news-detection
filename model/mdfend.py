659493e4eb3b844808abde0ad5e0683072b94d58
PCNinja <shiyuhui2000@126.com>
2022-01-12 15:10:22 +0800

    clear comments

--- model/mdfend.py
+++ model/mdfend.py
@@ -7,65 +7,6 @@ from tqdm import tqdm
 from functions import MLP, CNNExtractor, MaskAttention
 from utils import Averager, Recorder, split, metrics
 
-# class MultiDomainFENDModel(torch.nn.Module):
-#     def __init__(self, emb_dim, mlp_dims, task_num, bert_emb, dropout, emb_type ):
-#         super(MultiDomainFENDModel, self).__init__()
-#         self.domain_num = 9
-#         self.gamma = 10
-#         self.num_expert = 5
-#         self.fea_size =256
-#         self.emb_type = emb_type
-#         self.task_num = task_num
-#         if(emb_type == 'bert'):
-#             self.bert = BertModel.from_pretrained(bert_emb).requires_grad_(False)
-        
-#         feature_kernel = {1: 64, 2: 64, 3: 64, 5: 64, 10: 64}
-#         expert = []
-#         for i in range(self.num_expert):
-#             # expert.append(MLP(emb_dim, mlp_dims, dropout, False))
-#             expert.append(cnn_extractor(feature_kernel, emb_dim))
-#         self.expert = nn.ModuleList(expert)
-
-#         self.gate = nn.Sequential(Meta_Linear(emb_dim * 2 , mlp_dims[-1]),
-#                                       nn.ReLU(),
-#                                       Meta_Linear(mlp_dims[-1], self.num_expert),
-#                                       nn.Softmax(dim = 1))
-
-#         self.attention = MaskAttention(emb_dim)
-
-#         self.domain_embedder = Meta_Embedding(num_embeddings = self.domain_num, embedding_dim = emb_dim)
-#         self.specific_extractor = SelfAttentionFeatureExtract(multi_head_num = 1, input_size=emb_dim, output_size=self.fea_size)
-#         self.classifier = MLP(320, mlp_dims, dropout)
-        
-#     def get_param(self):
-#         return list(self.expert.parameters()) + list(self.gate.parameters()) + list(self.attention.parameters()) + list(self.domain_embedder.parameters()) + list(self.specific_extractor.parameters()) + list(self.classifier.parameters())
-    
-#     def forward(self, **kwargs):
-#         inputs = kwargs['content']
-#         masks = kwargs['content_masks']
-#         category = kwargs['category']
-#         if self.emb_type == "bert":
-#             init_feature = self.bert(inputs, attention_mask = masks)[0]
-#         elif self.emb_type == 'w2v':
-#             init_feature = inputs
-        
-#         feature, _ = self.attention(init_feature, masks)
-#         idxs = torch.tensor([index for index in category]).view(-1, 1).cuda()
-#         domain_embedding = self.domain_embedder(idxs).squeeze(1)
-
-#         gate_input_feature = feature
-#         gate_input = torch.cat([domain_embedding, gate_input_feature], dim = -1)
-#         gate_value = self.gate(gate_input)
-
-#         shared_feature = 0
-#         for i in range(self.num_expert):
-#             tmp_feature = self.expert[i](init_feature)
-#             shared_feature += (tmp_feature * gate_value[:, i].unsqueeze(1))
-
-#         label_pred = self.classifier(shared_feature)
-
-#         return torch.sigmoid(label_pred.squeeze(1))
-
 class Model(nn.Module):
     def __init__(self, domain_num, emb_dim=768, feature_kernel={1: 64, 2: 64, 3: 64, 5: 64, 10: 64}, hidden_dims=[512], dropout=0.2, expert_num=5, domain_emb_dim=768):
         super(Model, self).__init__()
