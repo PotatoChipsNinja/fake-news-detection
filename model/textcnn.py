550296f3bcc91a97c238ad67fd2bfb78dd44db91
PCNinja <shiyuhui2000@126.com>
2022-01-18 12:49:05 +0800

    optimize the framework

--- model/textcnn.py
+++ model/textcnn.py
@@ -1,11 +1,9 @@
 import os
 import torch
 from torch import nn, optim
-from transformers import BertModel
-from tqdm import tqdm
 
-from functions import CNNExtractor, MLP
-from utils import Averager, Recorder, split, metrics
+from utils.functions import CNNExtractor, MLP
+from utils.trainer import Trainer as TrainerBase
 
 class Model(nn.Module):
     def __init__(self, emb_dim=768, feature_kernel={1: 64, 2: 64, 3: 64, 5: 64, 10: 64}, hidden_dims=[512], dropout=0.2):
@@ -20,76 +18,29 @@ class Model(nn.Module):
         output = torch.sigmoid(output)
         return output
 
-class Trainer:
+class Trainer(TrainerBase):
     def __init__(self, device, pretrain_model, pretrain_dim, pretrain_dir, train_dataloader, val_dataloader, test_dataloader, epoch, lr, early_stop, model_save_dir, category_dict):
-        self.device = device
-        self.epoch = epoch
-        self.train_dataloader = train_dataloader
-        self.val_dataloader = val_dataloader
-        self.test_dataloader = test_dataloader
-        self.early_stop = early_stop
-        self.category_dict = category_dict
+        super(Trainer, self).__init__(device, pretrain_model, pretrain_dim, pretrain_dir, train_dataloader, val_dataloader, test_dataloader, epoch, lr, early_stop, model_save_dir, category_dict)
         self.model_save_path = os.path.join(model_save_dir, 'params_textcnn.pt')
         self.model = Model(emb_dim=pretrain_dim).to(device)
         self.criterion = nn.BCELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        self.bert = BertModel.from_pretrained(pretrain_model, cache_dir=pretrain_dir).to(device)
 
-    def train(self):
-        recorder = Recorder(self.early_stop)
-        for epoch in range(self.epoch):
-            print('----epoch %d----' % (epoch+1))
-            self.model.train()
-            avg_loss = Averager()
-            for i, batch in enumerate(tqdm(self.train_dataloader)):
-                input_ids = batch['input_ids'].to(self.device)
-                attention_mask = batch['attention_mask'].to(self.device)
-                feature = self.bert(input_ids, attention_mask).last_hidden_state.detach()
-                label = batch['label'].float().to(self.device)
-                self.optimizer.zero_grad()
-                output = self.model(feature)
-                output = output.squeeze()
-                loss = self.criterion(output, label)
-                loss.backward()
-                self.optimizer.step()
-                avg_loss.add(loss.item())
-            results = self.test(self.val_dataloader)
-            print('epoch %d: loss = %.4f, acc = %.4f, f1 = %.4f, auc = %.4f' % (epoch+1, avg_loss.get(), results['total']['accuracy'], results['total']['f1'], results['total']['auc']))
+    def get_loss(self, batch):
+        input_ids = batch['input_ids'].to(self.device)
+        attention_mask = batch['attention_mask'].to(self.device)
+        feature = self.bert(input_ids, attention_mask).last_hidden_state.detach()
+        label = batch['label'].float().to(self.device)
 
-            # early stop
-            decision = recorder.update(results['total']['f1'])
-            if decision == 'save':
-                torch.save(self.model.state_dict(), self.model_save_path)
-            elif decision == 'stop':
-                break
-            elif decision == 'continue':
-                continue
+        output = self.model(feature)
+        output = output.squeeze()
+        loss = self.criterion(output, label)
+        return loss
 
-        # load best model
-        self.model.load_state_dict(torch.load(self.model_save_path))
-        print('----test----')
-        results = self.test(self.test_dataloader)
-        print('test: acc = %.4f, f1 = %.4f, auc = %.4f' % (results['total']['accuracy'], results['total']['f1'], results['total']['auc']))
-
-    def test(self, dataloader):
-        self.model.eval()
-        category = torch.empty(0)
-        y_true = torch.empty(0)
-        y_score = torch.empty(0)
-        for i, batch in enumerate(tqdm(dataloader)):
-            input_ids = batch['input_ids'].to(self.device)
-            attention_mask = batch['attention_mask'].to(self.device)
-            feature = self.bert(input_ids, attention_mask).last_hidden_state.detach()
-            with torch.no_grad():
-                output = self.model(feature)
-            output = output.squeeze().cpu()
-            y_score = torch.cat((y_score, output))
-            y_true = torch.cat((y_true, batch['label']))
-            category = torch.cat((category, batch['category']))
-
-        results = dict()
-        results['total'] = metrics(y_true, y_score)
-        y_per_category = split(y_true, y_score, category, len(self.category_dict))
-        for category_name, category_id in self.category_dict.items():
-            results[category_name] = metrics(y_per_category[category_id][0], y_per_category[category_id][1])
-        return results
+    def get_output(self, batch):
+        input_ids = batch['input_ids'].to(self.device)
+        attention_mask = batch['attention_mask'].to(self.device)
+        feature = self.bert(input_ids, attention_mask).last_hidden_state.detach()
+        with torch.no_grad():
+            output = self.model(feature)
+        return output
---
+++ utils/trainer.py
@@ -0,0 +1,74 @@
+import torch
+from tqdm import tqdm
+from transformers import BertModel
+
+from utils.utils import Averager, Recorder, split, metrics
+
+class Trainer:
+    def __init__(self, device, pretrain_model, pretrain_dim, pretrain_dir, train_dataloader, val_dataloader, test_dataloader, epoch, lr, early_stop, model_save_dir, category_dict):
+        self.device = device
+        self.epoch = epoch
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.test_dataloader = test_dataloader
+        self.early_stop = early_stop
+        self.category_dict = category_dict
+        self.bert = BertModel.from_pretrained(pretrain_model, cache_dir=pretrain_dir).to(device)
+        # Need to define self.model_save_path, self.model, self.criterion and self.optimizer in derived class
+
+    def get_loss(self, batch):
+        # Need to be implemented in derived class
+        return None
+
+    def get_output(self, batch):
+        # Need to be implemented in derived class
+        return None
+
+    def train(self):
+        recorder = Recorder(self.early_stop)
+        for epoch in range(self.epoch):
+            print('----epoch %d----' % (epoch+1))
+            self.model.train()
+            avg_loss = Averager()
+            for i, batch in enumerate(tqdm(self.train_dataloader)):
+                self.optimizer.zero_grad()
+                loss = self.get_loss(batch)
+                loss.backward()
+                self.optimizer.step()
+                avg_loss.add(loss.item())
+            results = self.test(self.val_dataloader)
+            print('epoch %d: loss = %.4f, acc = %.4f, f1 = %.4f, auc = %.4f' % (epoch+1, avg_loss.get(), results['total']['accuracy'], results['total']['f1'], results['total']['auc']))
+
+            # early stop
+            decision = recorder.update(results['total']['f1'])
+            if decision == 'save':
+                torch.save(self.model.state_dict(), self.model_save_path)
+            elif decision == 'stop':
+                break
+            elif decision == 'continue':
+                continue
+
+        # load best model
+        self.model.load_state_dict(torch.load(self.model_save_path))
+        print('----test----')
+        results = self.test(self.test_dataloader)
+        print('test: acc = %.4f, f1 = %.4f, auc = %.4f' % (results['total']['accuracy'], results['total']['f1'], results['total']['auc']))
+
+    def test(self, dataloader):
+        self.model.eval()
+        category = torch.empty(0)
+        y_true = torch.empty(0)
+        y_score = torch.empty(0)
+        for i, batch in enumerate(tqdm(dataloader)):
+            output = self.get_output(batch)
+            output = output.squeeze().cpu()
+            y_score = torch.cat((y_score, output))
+            y_true = torch.cat((y_true, batch['label']))
+            category = torch.cat((category, batch['category']))
+
+        results = dict()
+        results['total'] = metrics(y_true, y_score)
+        y_per_category = split(y_true, y_score, category, len(self.category_dict))
+        for category_name, category_id in self.category_dict.items():
+            results[category_name] = metrics(y_per_category[category_id][0], y_per_category[category_id][1])
+        return results
